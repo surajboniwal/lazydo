91b1e7e89d693549b00f8828bdf5b3ddd84c2049
krish-bhanushali <bhanushali123krish@gmail.com>
2020-10-14 01:30:12 +0530

    Added Google Sign In methods

--- lib/helpers/firebase/firebase_methods.dart
+++ lib/helpers/firebase/firebase_methods.dart
@@ -1,1 +1,32 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
+class FirebaseMethods {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  GoogleSignInAccount googleUser;
+
+  //Future Method
+  Future<User> getUser() async {
+    return _auth.currentUser;
+  }
+
+  Future<UserCredential> signIn() async {
+    googleUser = await GoogleSignIn().signIn();
+    if (googleUser != null) {
+      GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      if (googleAuth.idToken != null && googleAuth.accessToken != null) {
+        final GoogleAuthCredential credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        );
+
+        return await _auth.signInWithCredential(credential);
+      }
+    }
+  }
+
+  Future<void> signOut() async {
+    await GoogleSignIn().signOut();
+    return await _auth.signOut();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lazydo/presentation/screens/splash/splash.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
