67f625e2871580219cfd9503c96ede727772c306
krish-bhanushali <bhanushali123krish@gmail.com>
2020-10-15 23:59:40 +0530

    Added Data model and updated Firebase methods also added Multidex Enabled

--- lib/helpers/firebase/firebase_methods.dart
+++ lib/helpers/firebase/firebase_methods.dart
@@ -1,9 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:lazydo/data/models/userDetails.dart';
 
 class FirebaseMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   GoogleSignInAccount googleUser;
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  static final CollectionReference _userCollection =
+      FirebaseFirestore.instance.collection('userDetails');
 
   //Future Method here if current user is null it means it logged out
   Future<User> getUser() async {
@@ -31,5 +36,44 @@ class FirebaseMethods {
   Future<void> signOutWithGoogle() async {
     await GoogleSignIn().signOut();
     return await _auth.signOut();
+  }
+
+  //Get user Details
+  Future<UserDetail> getUserDetails() async {
+    User currentUser = await getUser();
+    DocumentSnapshot documentSnapshot =
+        await _userCollection.doc(currentUser.uid).get();
+    return UserDetail.fromMap(documentSnapshot.data());
+  }
+
+  //Is user present in DB
+  Future<bool> authenticateUser(UserCredential userCredential) async {
+    QuerySnapshot results = await firestore
+        .collection('userDetails')
+        .where('email', isEqualTo: userCredential.user.email)
+        .get();
+    final List<DocumentSnapshot> docs = results.docs;
+
+    return docs.length == 0 ? true : false;
+  }
+
+  //Add User to DB
+
+  UserDetail userDetail = UserDetail();
+  Future<void> addDataToDb(UserCredential credential) async {
+    //Later generate dynamically
+    String username = 'LazyUser';
+
+    userDetail = UserDetail(
+        id: credential.user.uid,
+        email: credential.user.email,
+        displayName: credential.user.displayName,
+        profilePhoto: credential.user.photoURL,
+        userName: username);
+
+    firestore
+        .collection('userDetails')
+        .doc(credential.user.uid)
+        .set(userDetail.toMap());
   }
 }
